b22e3d4c77d6c4593a77f351108705df07e143f7
Michael Rook <michael@rook.net.au>
2022-06-21 16:52:27 +1000

    Micro opts

--- src/Theme.php
+++ src/Theme.php
@@ -2,6 +2,9 @@
 
 namespace BuildystrapChild;
 
+use function wp_enqueue_script;
+use function wp_enqueue_style;
+
 class Theme
 {
     /**
@@ -17,7 +20,7 @@ class Theme
         // Font Awesome
         // wp_enqueue_style('font-awesome', 'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css');
 
-        wp_enqueue_style('child-style', get_stylesheet_uri().'/dist/css/child-style.css', ['parent-style']);
+        wp_enqueue_style('child-style', get_stylesheet_uri() . '/dist/css/child-style.css', ['parent-style']);
     }
 
     /**
@@ -27,6 +30,6 @@ class Theme
      */
     public static function enqueue_scripts()
     {
-        wp_enqueue_script('child-script', get_stylesheet_uri().'/dist/js/child-script.js', ['parent-script']);
+        wp_enqueue_script('child-script', get_stylesheet_uri() . '/dist/js/child-script.js', ['parent-script']);
     }
 }
